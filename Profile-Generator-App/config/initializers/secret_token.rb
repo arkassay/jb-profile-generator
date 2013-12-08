7d6693e341121ac75077601e3699bc6b01c46fa1
Amanda <arkassay@gmail.com>
2013-12-08 18:33:07 -0500

    rails application

---
+++ Profile-Generator-App/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Profilegenerator::Application.config.secret_key_base = '5a8100dbc7eef5346f09a515c3e40c1d49ca868a08282dcc7537e60d718126723d3c83593a3970801cd8a0904c5ccf959061dbd839dfe157e9b0960ca53b78e0'
